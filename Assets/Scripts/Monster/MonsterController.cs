7d934b4ee252a06c1719a997d5b59ee456e83951
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-05-11 20:52:18 +0500

    tmp

--- Assets/Scripts/Monster/MonsterController.cs
+++ Assets/Scripts/Monster/MonsterController.cs
@@ -1,3 +1,4 @@
+using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -9,6 +10,8 @@ namespace Monster
         [FormerlySerializedAs("Speed")] public float speed;
         public Rigidbody2D rigitbody;
         public int fieldOfView;
+
+        private JobHandle handle;
         
         public void Start()
         {
@@ -20,11 +23,14 @@ namespace Monster
             var searcher = new Searcher(player, gameObject, fieldOfView * 2);
             var path = searcher.GetPathAStar();
             if (!(path is null))
+            {
+                var step = speed * Time.deltaTime;
                 foreach (var point in path)
-                {
-                    var step = speed * Time.deltaTime;
-                    transform.position = Vector2.MoveTowards(transform.position, new Vector2(point.x,point.y), step);
-                }
+                    transform.position = Vector2.MoveTowards(transform.position, 
+                        new Vector2(point.x, point.y), step);
+                transform.position = Vector2.MoveTowards(transform.position, 
+                    player.transform.position, step);
+            }
         }
 
         public void OnTriggerEnter2D(Collider2D other)
