72c1110b034f0cc3e04e484562120bfdb20039ed
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-05-26 11:40:05 +0500

    flamethrower ref

--- Assets/Scripts/FlameThrower.cs
+++ Assets/Scripts/FlameThrower.cs
@@ -11,13 +11,12 @@ public class FlameThrower : MonoBehaviour
     public float startTime;
 
     private AudioSource _audioSource;
-    // Start is called before the first frame update
+
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if (_timeShot <= 0)
