50dbb6e8a64175be3a9dd34ed19561b12d10c9f2
Polinariaa <polina.shch2@mail.ru>
2021-05-27 23:07:33 +0500

    try

--- Assets/Scripts/SManager.cs
+++ Assets/Scripts/SManager.cs
@@ -5,10 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SManager : MonoBehaviour
 {
-    public static LevelController instance = null;
-
-    private int sceneIndex;
-    public int levelComplete;
     public void GoToNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -18,18 +14,5 @@ public class SManager : MonoBehaviour
     public void GoToMenu()
     {
         SceneManager.LoadScene(1);
-    }
-    public void IsEndGame()
-    {
-        if (sceneIndex == 5)
-        {
-            Invoke("LoadMainMenu", 1f);
-        }
-        else
-        {
-            if (levelComplete < sceneIndex)
-                PlayerPrefs.SetInt("LevelComplete", sceneIndex + 1);
-            Invoke("NextLevel", 1f);
-        }
     }
 }
