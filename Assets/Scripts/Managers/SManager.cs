ba00eae654b6967b46041da164d52611fd5cd132
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-05-29 13:49:57 +0500

    удалила лишние скрипты

--- Assets/Scripts/Managers/SManager.cs
+++ Assets/Scripts/Managers/SManager.cs
@@ -21,4 +21,9 @@ public class SManager : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+    
+    public void SelectLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# GameVersion2
+# Earth Rescuer
+
+## Решение Game.sln  
+
+## Тесты в папке .\TestProjectAStar\AStar
