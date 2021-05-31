69394255a324f64ae9a6f471cd05504fb294885f
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-05-31 16:14:54 +0500

    rename 2 scripts

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SManager : MonoBehaviour
+public class ScenesManager : MonoBehaviour
 {
     public void GoToNextLevel()
     {
