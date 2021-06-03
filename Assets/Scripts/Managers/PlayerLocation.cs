d7a9104666e6eb70a4bde79c3d57be95560c3c20
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-06-03 23:35:03 +0500

    локация работает, но карту нужно поменять

---
+++ Assets/Scripts/Managers/PlayerLocation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLocation : MonoBehaviour
+{
+    public GameObject player;
+    public Transform leftCorner;
+    public Transform rightCorner;
+
+    public Image map;
+    public float mapImageWidth;
+    public float mapImageHeight;
+
+    private void Update()
+    {
+        var offsets = (Math.Abs(leftCorner.position.x - player.transform.position.x), 
+            Math.Abs(leftCorner.position.y - player.transform.position.y));
+        var levelMapWidth = rightCorner.position.x - leftCorner.position.x;
+        var levelMapHeight = rightCorner.position.y - leftCorner.position.y;
+
+        var (mapImageLeftCornerX, mapImageLeftCornerY) = (map.transform.position.x - mapImageWidth/2,
+            map.transform.position.y - mapImageHeight/2);
+
+        transform.position = new Vector2(mapImageLeftCornerX + offsets.Item1 * mapImageWidth / levelMapWidth,
+            mapImageLeftCornerY + offsets.Item2 * mapImageHeight / levelMapHeight);
+    }
+}
