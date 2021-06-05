0ddc9f0bded3e057d195aefeaed2760d84899d7c
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-06-05 23:32:50 +0500

    tmp

--- Assets/Scripts/Managers/PlayerLocation.cs
+++ Assets/Scripts/Managers/PlayerLocation.cs
@@ -6,24 +6,18 @@ using UnityEngine.UI;
 public class PlayerLocation : MonoBehaviour
 {
     public GameObject player;
-    public Transform leftCorner;
-    public Transform rightCorner;
-
-    public Image map;
-    public float mapImageWidth;
-    public float mapImageHeight;
+    public Transform[] gameFlags;
+    public Transform[] minimapLocation;
 
     private void Update()
     {
-        var offsets = (Math.Abs(leftCorner.position.x - player.transform.position.x), 
-            Math.Abs(leftCorner.position.y - player.transform.position.y));
-        var levelMapWidth = rightCorner.position.x - leftCorner.position.x;
-        var levelMapHeight = rightCorner.position.y - leftCorner.position.y;
-
-        var (mapImageLeftCornerX, mapImageLeftCornerY) = (map.transform.position.x - mapImageWidth / 2,
-            map.transform.position.y - mapImageHeight / 2);
-
-        transform.position = new Vector2(mapImageLeftCornerX + offsets.Item1 * mapImageWidth / levelMapWidth,
-            mapImageLeftCornerY + offsets.Item2 * mapImageHeight / levelMapHeight);
+        for (var i = 0; i < gameFlags.Length; i++)
+        {
+            if ((player.transform.position - gameFlags[i].position).magnitude <= 15)
+            {
+                transform.position = minimapLocation[i].position;
+                break;
+            }
+        }
     }
 }
