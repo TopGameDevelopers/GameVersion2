f697fc54d127ce0aa08ff57cec644a5c69777456
Ksyusha <astakhova.ksyusha.07@mail.ru>
2021-06-05 21:30:21 +0500

    работает пауза на марсе

--- Assets/Scripts/Managers/PlayerLocation.cs
+++ Assets/Scripts/Managers/PlayerLocation.cs
@@ -20,11 +20,8 @@ public class PlayerLocation : MonoBehaviour
         var levelMapWidth = rightCorner.position.x - leftCorner.position.x;
         var levelMapHeight = rightCorner.position.y - leftCorner.position.y;
 
-        var (mapImageLeftCornerX, mapImageLeftCornerY) = (map.transform.position.x - mapImageWidth/2,
-            map.transform.position.y - mapImageHeight/2);
-        
-        Debug.Log(mapImageLeftCornerX);
-        Debug.Log(mapImageLeftCornerY);
+        var (mapImageLeftCornerX, mapImageLeftCornerY) = (map.transform.position.x - mapImageWidth / 2,
+            map.transform.position.y - mapImageHeight / 2);
 
         transform.position = new Vector2(mapImageLeftCornerX + offsets.Item1 * mapImageWidth / levelMapWidth,
             mapImageLeftCornerY + offsets.Item2 * mapImageHeight / levelMapHeight);
